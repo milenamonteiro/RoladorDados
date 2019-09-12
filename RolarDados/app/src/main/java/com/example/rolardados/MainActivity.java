e67c8833b0a0739777b440f9005dc6c5d042998f
Milena Monteiro <milenamonteiro2002@hotmail.com>
2019-09-12 19:37:40 -0300

    Final

--- RolarDados/app/src/main/java/com/example/rolardados/MainActivity.java
+++ RolarDados/app/src/main/java/com/example/rolardados/MainActivity.java
@@ -1,35 +1,18 @@
 package com.example.rolardados;
-
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
-    int counter = 0;
-    ArrayList<EditText> etnumlados = new ArrayList<>();
-    ArrayList<EditText> etvezes = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void verificarDados(View view){
-        EditText etnumlados = (EditText)findViewById(R.id.lados);
-        EditText etvezes = (EditText)findViewById(R.id.vezes);
-        EditText etvalormod = (EditText)findViewById(R.id.modf);
-        RadioGroup rgroup = (RadioGroup)findViewById(R.id.radio);
+        EditText etnumlados = findViewById(R.id.lados);
+        EditText etvezes = findViewById(R.id.vezes);
+        EditText etvalormod = findViewById(R.id.modf);
+        RadioGroup rgroup = findViewById(R.id.radio);
 
         String numlados = etnumlados.getText().toString(), vezes = etvezes.getText().toString(), valormod = etvalormod.getText().toString();
 
@@ -70,35 +53,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void rolarDado(){
-        EditText etnumlados = (EditText)findViewById(R.id.lados);
-        EditText etvezes = (EditText)findViewById(R.id.vezes);
-        EditText etvalormod = (EditText)findViewById(R.id.modf);
+        EditText etnumlados = findViewById(R.id.lados);
+        EditText etvezes = findViewById(R.id.vezes);
+        EditText etvalormod = findViewById(R.id.modf);
 
         double numlados = Double.parseDouble(etnumlados.getText().toString());
         Integer vezes = Integer.parseInt(etvezes.getText().toString());
         Integer valormod = Integer.parseInt(etvalormod.getText().toString());
 
         double min = 1;
-        int result, resultmod;
-        vezes++;
+        int result, resultmod, somaresults = 0;
+        List<Integer> valores = new ArrayList<>();
 
-        String resultados = new String();
+        String resultados, tipomod = "";
 
         for (int i = 1; i <= vezes; i++)
         {
             min = Math.ceil (1);
             numlados = Math.floor (numlados);
             result = (int) (Math.floor (Math.random () * (numlados)) + min);
-            resultados += String.format("\n\nResultado %o = %o%n", i, result, valormod);
             if(boolmod){
                 resultmod = result + valormod;
-                resultados += String.format("Modificador = + %o%nFinal = %o", valormod, resultmod);
+                tipomod = "+";
             }
             else{
-                resultmod = result - valormod;
-                resultados += String.format("Modificador = - %o%nFinal = %o", valormod, resultmod);
+                resultmod = (result - valormod);
+                tipomod = "-";
             }
+            somaresults += resultmod;
+            valores.add(resultmod);
         }
+        resultados = String.format("%nDado%n%sd%s%s%s%n%nFinal%n%s%n%nValores%n", vezes, (int)numlados, tipomod, valormod, somaresults);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int a : valores){
+            sb.append(a).append("; ");
+        }
+
+        resultados += (sb.toString());
 
         Intent sendIntent = new Intent(MainActivity.this, ResultActivity.class);
         sendIntent.putExtra(Intent.EXTRA_TEXT, resultados);
